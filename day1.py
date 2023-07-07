926d7a9542b1b002400684044a5d53aee8778f01
palandeshreya25 <138582189+palandeshreya25@users.noreply.github.com>
2023-07-07 09:51:18 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter the marks for subject 1: "))
+n2 = int(input("Enter the marks for subject 2: "))
+n3 = int(input("Enter the marks for subject 3: "))
+
+average = (n1 + n2 + n3) / 3
+
+print(f"The average of {n1}, {n2}, and {n3} is {average}")

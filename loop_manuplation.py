27d137e517d644d6cf9a5fe98dc0002f2727aefc
palandeshreya25 <138582189+palandeshreya25@users.noreply.github.com>
2023-07-11 21:51:20 +0530

    Add files via upload

--- loop_manuplation.py
+++ loop_manuplation.py
@@ -1,32 +1,38 @@
-#break
-for i in range(1, 10):
-    if i == 6:
-        break
-    print(i)
+choice = input("\
+ Options\n\
+ 1 = break\n\
+ 2 = pass \n\
+ 3 = continue\n\
+ 4 = for with else\n\
+ 5 = while with else \n\
+Choose an option :")
 
-#pass
-for i in range(1, 10):
-    if i == 6:
-        pass
-    else:
+if choice == "1":
+    for i in range(10):
+        if i == 5:
+            break
         print(i)
-
-#continue
-for i in range(1, 10):
-    if i == 6:
-        continue
-    print(i)
-
-# for loop with else statement
-for i in range(1, 11):
-    print(i)
+elif choice == "2":
+    for i in range(5):
+        pass
+elif choice == "3":
+    for i in range(5):
+        if i == 3:
+            continue
+        print(i)
+elif choice == "4":
+    for i in range(5):
+        print(i)
+    else:
+        print("Loop completed!")
+elif choice == "5":
+    i = 0
+    while i < 5:
+        print(i)
+        i += 1
+    else:
+        print("Loop completed!")
 else:
-    print("Loop completed successfully!")
+    print("Invalid choice!")
 
-#while loop with else statement
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
-else:
-    print("Loop completed successfully!")
+
